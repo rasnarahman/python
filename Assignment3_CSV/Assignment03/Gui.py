8914ed2c430ddba9a0f4dce016d0149b47fdd53d
Rasna Rahman <rahman.rahman@gmail.com>
2018-10-20 02:26:29 -0400

    sorting

--- Assignment3_CSV/Assignment03/Gui.py
+++ Assignment3_CSV/Assignment03/Gui.py
@@ -1,24 +1,39 @@
+"""CST8333: Assignment 03 – Proof of Concept Delivery
+   Author: Rasna Rahman
+   Date: October 10, 2018
+   this class displaying the windows and displaying the .csv file."""
+from tkinter import Tk, Button, Frame
+from Assignment03 import ReadDataSetFile
+import tkinter as tk
 
-from tkinter import Tk, Label, Button, StringVar, Frame, TOP, NO, NONE, LEFT
-from Assignment03 import file_Read
 
+class DisplayDataSetGUI:        #created the DisplayDataSetGUI class
 
-class DisplayDataSetGUI:
-
-    def __init__(self, master):
+    def __init__(self, master):  # constructor of the class
         self.master = master
-        master.wm_title('TestDataSetGUI' )
-        frame = Frame(master)
+        master.wm_title('Programming Language Research Project')
+        frame = Frame(master)  # frame created
         frame.pack()
-        master.geometry("300x100")
+        master.geometry("500x150")    # window size is set.
 
-        fileReadLoadData = file_Read.load_Data()
-        Button(frame, text='Open File', width=20, command=lambda: fileReadLoadData.load_data()).grid(row=3, column=0)
+        v = tk.StringVar ()
+        tk.Label ( root,text="""Select Sort Property:""").pack ()
+        tk.Radiobutton ( root,text="REF_DATE",padx=200,variable=v,value="REF_DATE" ).pack ( anchor=tk.W )
+        tk.Radiobutton ( root,text="COMMODITY",padx=200,variable=v,value="COMMODITY" ).pack ( anchor=tk.W )
+        tk.Radiobutton ( root,text="VALUE",padx=200,variable=v,value="VALUE" ).pack ( anchor=tk.W )
 
-        self.close_button = Button(frame, text="Exit", width=20, command=root.destroy)
+        fileReadLoadData = ReadDataSetFile.ReadDataSetFile()
+        Button(frame, text='Open File', width=20, command=lambda: fileReadLoadData.load_data(v.get())).grid(row=3, column=0)   # Button to open the file
+        #Button (frame,text='Filter by Available Food ',width=10,command=lambda: fileReadLoadData.threadstarter('filter','Food available')).grid ( row=3,column=1)
+        #Button (frame,text='Food adjusted for losses',width=10,command=lambda: fileReadLoadData.threadstarter ( 'filter','Food available adjusted for losses' ) ).grid ( row=3,column=2)
+
+
+
+
+        self.close_button = Button(frame, text="Exit", width=20, command=root.destroy)                 #close Button
         self.close_button.grid(row=4, column=0)
 
 
-root = Tk()
-my_gui = DisplayDataSetGUI(root)
-root.mainloop()
+root = Tk()                        #instantiate root
+my_gui = DisplayDataSetGUI(root)   #instantiate my_gui
+root.mainloop()                    #start to display
---
+++ Assignment3_CSV/Assignment03/ReadDataSetFile.py
@@ -0,0 +1,60 @@
+"""CST8333: Assignment 03 – Proof of Concept Delivery
+   Author: Rasna Rahman
+   Date: October 10, 2018
+   this class opening the .csv file and loading it in a label."""
+
+import tkinter  # import the tkinter
+import csv  # import the csv
+from tkinter import *
+from tkinter.filedialog import askopenfilename  # import the method to open the file(Dialog Box)
+
+class ReadDataSetFile:  # class ReadDataSetFile created
+    def __init__(self):  # constructor
+        self
+
+    def load_data(self, sortProperty):                    # method to read the .csv file and load 6 columns
+        csv_file_path = askopenfilename()
+
+        tk = tkinter.Tk()
+        self.close_button = Button(tk, text="Close", width=10, command=tk.destroy).pack(side=BOTTOM)  # Button to close the window
+        scrollbar = Scrollbar(tk)                       # Scrollbar to scroll the data file
+        scrollbar.pack(side=RIGHT, fill=Y)
+        tk.geometry("700x550")                          # size for the open window.
+
+        lb1 = Listbox(tk, yscrollcommand=scrollbar.set, width=100)  # created the list box to hold the data
+        i = 0
+        try:                                                  # try catch block to handle the exceptions.
+
+            with open(csv_file_path) as dataSet:
+                reader = csv.reader(dataSet, delimiter=',')
+
+                print("Sort property: " + sortProperty)
+                if sortProperty == "REF_DATE":
+                    reader = sorted ( reader,key=lambda row: row[0],reverse=True )
+                elif sortProperty == "COMMODITY":
+                    reader = sorted ( reader,key=lambda row: row[4],reverse=True )
+                elif sortProperty == "VALUE":
+                    reader = sorted ( reader, key=lambda row: row[11], reverse=True )
+
+                for row in reader:
+                    if i == 0: # Skip the first row and print title
+                        data = "YEAR  -  COUNTRY  -  COMMODITY  -  VECTOR  -  COORDINATE  -  VALUE  -  CATEGORIES "
+
+                        lb1.insert(i, data)
+                        i = i + 1
+                        continue
+                    year = row[0]
+                    country = row[1]
+                    commodity = row[4]
+                    vector = row[9]
+                    coordinate = row[10]
+                    categories = row[3]
+                    value = row[11]
+                    data = year + " - " + country + " - " + commodity + " - " + vector + " - " + coordinate + " - " + value+" - "+ categories
+                    lb1.insert(i, data)   # print the rows
+                    i = i + 1
+                lb1.pack(side=LEFT, fill=BOTH)
+            tk.mainloop()
+            dataSet.close()
+        except IOError:
+            print("could not open the file:")
